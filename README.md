70f70c636a285394ed5c0d3cea679dcef35fc945
Hein Htet Aung <72753389+heinhtet8X@users.noreply.github.com>
2025-08-08 23:38:16 +0700

    setup

---
+++ README.md
@@ -0,0 +1,5 @@
+#Laravel 12 Moduler 
+
+https://www.souysoeng.com/2025/02/laravel-modules-introduction.html.html
+--
+full setup
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+// Route::prefix('v1')->group(function(){
+
+//     Route::prefix('auth')->group(function(){
+//         Route::post('/register');
+//         Route::post('/login');
+//     });
+// });
